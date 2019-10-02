33f423108ecbb769144bd77b25fb41c4e056c840
Alexander Borodich <sasha.barodach@gmail.com>
2019-10-02 16:56:04 +0300

    feat: implement function decode

--- src/index.js
+++ src/index.js
@@ -38,7 +38,84 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+
+		let sign = [];
+		let result = '';
+		let i = 0;	//count sign
+		let letter = [];
+		let l = 0;	//count letter
+		sign[0] = '';
+		
+		for (let j = 0; j < expr.length; j = j + 2) {
+
+			if ((expr[j] + expr[j+1]) == '10') {
+				sign[i] += '.';
+			}
+
+			if ((expr[j] + expr[j+1]) == '00') {
+				while (expr[j] == '0') {
+					j++;
+				}
+				j-=2;
+				if (sign[i] != '') {
+					i++;
+					sign[i] = '';
+				}
+			}
+
+			if ((expr[j] + expr[j+1]) == '11') { 
+				sign[i] += '-';
+			}
+
+			if ((expr[j] + expr[j+1]) == '**') { 
+				while (expr[j] == '*') {
+					j++;
+				}
+				j-=2;
+
+				i++;
+				sign[i] = ' ';
+				sign[++i] = '';
+			}
+		}
+		
+		for (let i = 0, l = 0; i < sign.length; i++) {	
+
+			if (Math.floor(sign[i].length / 5 > 0) && sign[i].length > 5) {
+				let remainder = sign[i].length % 5;
+				let count = Math.floor(sign[i].length / 5);
+				
+				for (let y = 0, from = 0; y < count+1; y++) {
+					letter[l] = sign[i].slice(from, remainder);
+					from = remainder;
+					remainder += 5;
+					l++;
+				}
+				
+			} else {
+				letter[l] = sign[i];
+				l++;
+			}
+		}
+		
+		for (let l = 0; l < letter.length; l++) {
+
+
+			if (letter[l] == ' ') {
+				result += ' ';
+				continue;
+			}
+
+			for (let key in MORSE_TABLE) {
+				if (key == letter[l]) {
+					result += MORSE_TABLE[key];
+				}
+			}
+
+		}
+
+		return result;
+
 }
 
 module.exports = {
